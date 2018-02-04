288214fdf3f4c8338c6e4fdd2af3ec9f2a64b82f
yukoamino <y-amino@y-amino.local>
2018-02-04 15:33:27 +0900

    CSS設計

---
+++ practice/css-design/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  gulp.src('./resource/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./htdocs/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./resource/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch']);
