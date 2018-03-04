69cd2d947f96cdd1c84616cc3ee1b61618f48602
yukoamino <y-amino@y-amino.local>
2018-03-04 12:07:58 +0900

    絞り込み

---
+++ practice/squeeze/htdocs/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+	var $list = $('.js-squeeze-list ul');
+	$('.js-squeeze-select select').change(function(){
+		var val = $(this).val();
+
+		$list.fadeOut();
+		$list.find('li').each(function(){
+			$(this).hide();
+			if(val == $(this).data('category') || val == ''){
+				$(this).show();
+			}
+		});
+		$list.fadeIn();
+	});
+});
