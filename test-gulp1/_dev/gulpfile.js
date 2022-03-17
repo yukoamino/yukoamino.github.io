ac9a6ec2b4874a2eb2e820125a6a7a0ddc7bef29
yukoamino <y-amino@imjp.co.jp>
2022-03-17 14:13:55 +0900

    test-gulp1 pug

--- test-gulp1/_dev/gulpfile.js
+++ test-gulp1/_dev/gulpfile.js
@@ -4,6 +4,13 @@ var plumber         = require('gulp-plumber');
 var notify          = require("gulp-notify");
 var pug             = require('gulp-pug');
 
+var fs             = require('fs');
+const data   = require('gulp-data');
+// const rename = require('gulp-rename');
+
+// var template = './pug/card/test.pug';
+var jsonPath = './pug/pug-prod.json';
+
 /*--------------------------------------------------------*/
 gulp.task('pug', function (){
   gulp
@@ -13,6 +20,28 @@ gulp.task('pug', function (){
     .pipe(plumber({ errorHandler: notify.onError("pug Error\n <%= error.message %>") }))
     .pipe(pug({pretty: util.env.d ? true : true}))
     .pipe(gulp.dest('../'));
+});
+
+/**************************************************
+ * task
+ *************************************************/
+gulp.task('pug-prod', () => {
+  const json = JSON.parse(fs.readFileSync(jsonPath));
+
+  for (let key of json) {
+    gulp.src(key.template)
+      // .pipe(rename(key.no + ".html"))
+      .pipe(data (function (file){
+        return {
+          'fileName': file.path.split('/').pop().replace('.html', ''),
+          'planList': json
+        }
+      }))
+      .pipe(pug({
+        pretty: true
+      }))
+      .pipe(gulp.dest('../' + key.directory + '/'));
+  }
 });
 
 gulp.task('watch', function () {
