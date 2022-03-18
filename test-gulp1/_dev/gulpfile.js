37d987c2a5d8b431e1648f761c5935e3a254718b
yukoamino <y-amino@imjp.co.jp>
2022-03-18 14:24:23 +0900

    test-gulp1 scss

--- test-gulp1/_dev/gulpfile.js
+++ test-gulp1/_dev/gulpfile.js
@@ -1,15 +1,53 @@
-var gulp            = require('gulp');
-var util            = require('gulp-util');
-var plumber         = require('gulp-plumber');
-var notify          = require("gulp-notify");
-var pug             = require('gulp-pug');
+var gulp = require('gulp'),
+  autoprefixer = require('gulp-autoprefixer'),
+  notify = require('gulp-notify'),
+  plumber = require('gulp-plumber'),
+  sass = require('gulp-sass')(require('sass')),
+  fs = require('fs'),
+  util = require('gulp-util'),
 
-var fs             = require('fs');
-var data           = require('gulp-data');
+  pug = require('gulp-pug'),
+  data = require('gulp-data');
 
 var jsonPath = './pug/pug-prod.json';
 
-/*--------------------------------------------------------*/
+//------------------------------------------------------------------------------
+// scss
+//------------------------------------------------------------------------------
+gulp.task('sp:sass', () => {
+  return gulp
+    .src(__dirname + '/scss/**/*.scss')
+    .pipe(
+      plumber({
+        errorHandler: notify.onError('<%= error.message %>'),
+      })
+    )
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+      }).on('error', sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: [
+          'last 2 versions',
+          'ie >= 9',
+          'ChromeAndroid >= 6',
+          'Android >= 6',
+          'iOS >= 9',
+        ],
+      })
+    )
+    .pipe(gulp.dest('../assets/css'));
+});
+
+gulp.task('sp:watch', () => {
+  gulp.watch(__dirname + '/scss/**/*.scss', ['sp:sass']);
+});
+
+//------------------------------------------------------------------------------
+// pug
+//------------------------------------------------------------------------------
 gulp.task('pug', function (){
   gulp
     .src([
@@ -48,4 +86,4 @@ gulp.task('watch-prod', function () {
   gulp.watch(['./pug/_**/*.pug'], ['pug-prod']);
 });
 
-gulp.task('default', ['watch','pug','pug-prod','watch-prod']);
+gulp.task('default', ['watch','pug','pug-prod','watch-prod','sp:watch']);
